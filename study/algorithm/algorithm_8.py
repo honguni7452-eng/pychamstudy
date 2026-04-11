1482c0aa0099be97a803ba1e7300385028aee07d
hongu <honguni@naver.com>
2026-04-11 15:53:01 +0900

    deque, dict

---
+++ study/algorithm/algorithm_8.py
@@ -0,0 +1,28 @@
+#1158번 유럽여행
+# deque, 딕셔너리
+
+from collections import deque
+
+N, K = map(int, input().split())
+
+queue = deque()
+for i in range(K, N + 1):
+    queue.append(i)
+for i in range(1, K):
+    queue.append(i)
+
+answer = []
+while True:
+    answer.append(queue.popleft())
+
+    if len(queue) == 0:
+        break
+
+    for _ in range(K - 1):
+        u = queue.popleft()
+        queue.append(u)
+
+print("<", end="")
+for i in range(N-1):
+    print(answer[i], end=", ")
+print(answer[N - 1], end=">")
