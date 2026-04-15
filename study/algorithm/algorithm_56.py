21af6665a76eec938cdd5bb2005db6580beba571
hongu <honguni@naver.com>
2026-04-15 11:29:13 +0900

    정렬 알고리즘

---
+++ study/algorithm/algorithm_56.py
@@ -0,0 +1,16 @@
+# 16466번 콘서트
+# 정렬
+
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+found = False
+for i in range(N):
+    if A[i] != (i + 1):
+        print(i + 1)
+        found = True
+        break
+
+if not found:
+    print(N + 1)
