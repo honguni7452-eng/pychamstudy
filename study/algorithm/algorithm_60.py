e2a3c9f856afdbcaab7b254db14fcdb2110c70e6
hongu <honguni@naver.com>
2026-04-21 18:48:04 +0900

    브루트포스 알고리즘

---
+++ study/algorithm/algorithm_60.py
@@ -0,0 +1,25 @@
+# easy 60. 17286번 유미
+# 브루트포스
+
+from itertools import permutations
+
+X = []
+Y = []
+for _ in range(4):
+    x, y = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+
+min_move = le9
+for permutation in permutations([1, 2, 3], 3):
+    x = X[0]
+    y = Y[0]
+
+    move = 0
+    for i in permutation:
+        move += abs(x - X[i]) ** 2 + abs(y - Y[i]) ** 0.5
+        x = X[i]
+        y = Y[i]
+
+    min_move = min(min_move, int(move))
+print(min_move)
