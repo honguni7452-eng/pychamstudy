a82e0ec2a0344be0cc989be5fbfb1d389d116f22
hongu <honguni@naver.com>
2026-04-23 19:42:35 +0900

    브루트포스 알고리즘

---
+++ study/algorithm/algorithm_64.py
@@ -0,0 +1,15 @@
+N = int(input())
+A = [0] * N
+B = [0] * N
+for i in range(N):
+    A[i], B[i] = map(int, input().split())
+
+min_time = 1e9
+for i in range(N):
+    if A[i] <= B[i]:
+        min_time = min(min_time, B[i])
+
+if min_time == 1e9:
+    print(-1)
+else:
+    print(min_time)
