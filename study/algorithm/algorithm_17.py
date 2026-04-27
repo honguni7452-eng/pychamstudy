31e6dc039fa70988862e5f8c1b57680c59606718
hongu <honguni@naver.com>
2026-04-27 18:34:44 +0900

    브루트포스 알고리즘

---
+++ study/algorithm/algorithm_17.py
@@ -0,0 +1,33 @@
+# 14889번 스타트와 링크
+# 브루트포스 알고리즘
+
+N = int(input())
+S = [[] for _ in range(N)]
+
+for i in range(N):
+    S[i] = list(map(int, input().split()))
+
+min_diff = 1e9
+for combination in combinations(list(range(N)), N/2):
+    team_a = combination
+    team_b = []
+    for i in range(N):
+        if i not in team_a:
+            team_b.append(i)
+
+    power_a = 0
+    power_b = 0
+
+    for x in team_a:
+        for y in team_a:
+            power_a += S[x][y]
+
+    for x in team_b:
+        for y in team_b:
+            power_b += S[x][y]
+
+    diff = abs(power_a - power_b)
+    if min_diff > diff:
+        min_diff = diff
+
+print(diff)
