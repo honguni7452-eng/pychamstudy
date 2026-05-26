77eb3b9f1f8f0befa05ee141f216af91f6ba953b
hongu <honguni@naver.com>
2026-05-26 19:18:40 +0900

    BST 성질

---
+++ study/grind/grind2_2.py
@@ -0,0 +1,16 @@
+# 235. Lowest Common Ancestor of a Binary Search Tree
+# BST 성질
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        cur = root
+
+        while cur:
+            if p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+
+            elif p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+
+            else:
+                return cur
