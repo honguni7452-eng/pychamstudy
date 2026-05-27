e34cff312f1d48022495168b5b5014ed9122f2cb
hongu <honguni@naver.com>
2026-05-27 18:13:14 +0900

    투 포인터

---
+++ study/grind/grind2_4.py
@@ -0,0 +1,16 @@
+# 141. Linked List Cycle
+# 투 포인터
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
